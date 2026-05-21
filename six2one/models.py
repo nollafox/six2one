831903586591117e1226bb8ebb0c34b6e1c05bc0
Nolla Fox <nollafox.dev@gmail.com>
2026-05-21 15:27:48 -0600

    v0.3.0

--- six2one/models.py
+++ six2one/models.py
@@ -8,7 +8,7 @@ from typing import Final
 
 DEFAULT_OUTPUT_DIR: Final = Path("./output")
 TOOL_NAME: Final = "six2one"
-TOOL_VERSION: Final = "0.2.0"
+TOOL_VERSION: Final = "0.3.0"
 
 
 class Site(str, Enum):
